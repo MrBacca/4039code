5f3dd695187d81cdc1efae6a8dfd04d74bc18d87
Corey Bastarache <bastarc@mcmaster.ca>
2015-10-20 16:35:56 -0400

    Continued work on angle correction (not done)

--- SmartGyro.cpp
+++ SmartGyro.cpp
@@ -13,6 +13,19 @@ float SmartGyro::getAngle(void)
 
 void SmartGyro::tick(void)
 {
+	for (int i = 9 ; i > 0 ; i --)
+	{
+		lastEncoder[i] = lastEncoder[i - 1];
+		lastGyro[i] = lastGyro[i - 1];
+	}
+	
+	lastEncoder[0] = (m_leftEncoder->getValue() + m_rightEncoder->getValue) / 2;
+	
+	lastGyro[0] = Gyro :: getAngle();
+	
+	if (m_leftEncoder + m_rightEncoder / 2) =
+	m_angle = m_angle + (Gyro + x); 
+
 }
 
 void SmartGyro::setSensitivity(float sensitivity)
